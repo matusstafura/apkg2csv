c692b131f21bb227a2ab92023ad22bf7d66f1471
Matus <kobe21@gmail.com>
2022-02-14 15:53:19 +0100

    sqlite, read db

--- script.py
+++ script.py
@@ -1,15 +1,33 @@
+import sqlite3
 from zipfile import ZipFile 
 
 def file():
-
     try:
         with ZipFile('temp.apkg', 'r') as apkg_file:
             file = apkg_file.read('collection.anki2')
-            with open('daco.csv', 'wb') as f:
+            with open('tempfile', 'wb') as f:
                 f.write(file)
 
     except Exception as e:
         print(e)
+        
+file()
 
+def connect(db_file):
+    conn = None
+    try:
+        conn = sqlite3.connect(db_file)
+    except Exception as e:
+        print(e)
+    return conn
 
-print(file())
+def selectData(conn):
+    cur = conn.cursor()
+    cur.execute("SELECT flds,sfld FROM notes")
+    rows = cur.fetchall()
+    
+    for row in rows:
+        print(row[0].split("\x1f"))
+        
+x = connect('tempfile')
+selectData(x)
