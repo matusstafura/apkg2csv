d8b0acfa6bdbe7e0345b7e1e7bb8e4ac5f21d5be
Matus Stafura <kobe21@gmail.com>
2022-11-01 19:02:02 +0100

    tests: values

--- tests/TestFile.py
+++ tests/TestFile.py
@@ -1,13 +1,26 @@
+from string import capwords
 import unittest
 import sys
 
 sys.path.append("src")
-from script import header, connect
+from script import header, connect, values
 
 class TestFile(unittest.TestCase):
     def test_header(self):
         x = header(connect('tests/tempfile'))
         self.assertEqual(['English', 'French', 'Word Type', 'Example', 'Example in English'],x)
+    
+    def test_values(self):
+        cards = values(connect('tests/tempfile'))
+        self.assertEqual([
+            ['cat', 'le chat', 'noun', 'je vois le chat', 'I see the cat'],
+            ['chien', 'le chien', 'noun', 'le chien me voit', 'the dog sees me']], 
+        cards)
+    
+    def test_file(self):
+        notAFile = connect('tests/temp.apkg')
+        self.assertRaises(Exception,notAFile)
+
 
 if __name__ == '__main__':
     unittest.main()
